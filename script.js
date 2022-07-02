a2d64b06d81e29cbc1697068fd8aa57f4a490c58
Himeth Dahanayake <himethdaha@gmail.com>
2022-07-02 13:47:05 -0400

    Adding the models

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const webcam = document.querySelector("#webcam");
 
+//Loading all the models
+Promise.all([
+  //Faster, smaller less resource consuming detector
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  //Detect parts of the face(eyes,nose and etc)
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  //Detect where the face is
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  //Used to detect emotions
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]);
 //Function to hookup the webcam with the video container
 function hookWebCam() {
   navigator.mediaDevices
